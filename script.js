9feb5fd65bfeb246422ef3d4ed2e3b23d78a590e
wanderer-00 <56039464+wanderer-00@users.noreply.github.com>
2022-02-06 04:33:24 +0300

    fi:rst,ix

---
+++ script.js
@@ -0,0 +1,38 @@
+// Стрелки ← ↑ → ↓ △ ▽ ◁ ▷
+const cluster = document.getElementById('cluster');
+let hp = [];
+
+function SSS(){
+    if (1==1) { return 1 } else { return 0 };
+};
+
+
+const load = async () => {
+    try {
+        const res = await fetch('URLs.json');
+        hp = await res.json();
+        console.log(hp);
+        //console.log("img = ", hp[5]['img']);
+        displayCharacters(hp);
+    } catch (err) {
+        //console.error(err);
+        console.log('ERROR');
+    }
+};
+
+const displayCharacters = (characters) => {
+    const htmlString = characters.map((character) => {
+        return`
+        <a href='${character.URL}'>
+            <div class="box">
+                <div style='background: url(ICONs/${character.ICON}) 0 0 /100% no-repeat' class='icon'></div>
+                <div class='title'>COUB</div>
+            </div>
+        </a>\n`
+        ;
+        }).join('');
+    console.log(htmlString);
+    cluster.innerHTML = htmlString;
+};
+
+load();
